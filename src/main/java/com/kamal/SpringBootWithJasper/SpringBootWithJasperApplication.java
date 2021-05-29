7661e8649422ed181cf84ff7738a9d8fc362c304
kamal_hossain <sony@gmail.com>
2021-05-29 12:24:34 +0600

    Initial commit

---
+++ src/main/java/com/kamal/SpringBootWithJasper/SpringBootWithJasperApplication.java
@@ -0,0 +1,13 @@
+package com.kamal.SpringBootWithJasper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithJasperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithJasperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamal/SpringBootWithJasper/SpringBootWithJasperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamal.SpringBootWithJasper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithJasperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
